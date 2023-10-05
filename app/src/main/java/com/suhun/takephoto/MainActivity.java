bc77243116148eb30eec1c4e0ced14508ea3f514
Shu-Hung Lin <suhunlin0310@gmail.com>
2023-10-05 08:47:19 +0800

    Add layout

--- app/src/main/java/com/suhun/takephoto/MainActivity.java
+++ app/src/main/java/com/suhun/takephoto/MainActivity.java
@@ -3,12 +3,22 @@ package com.suhun.takephoto;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    private String tag = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void takePhotoNotSaveToDevice(View view){
+
+    }
+
+    public void takePhotoSaveToDevice(View view){
+
     }
 }
